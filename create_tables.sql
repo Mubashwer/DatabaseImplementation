3bd090a18a63aaaebfae84184816abf8b4cd5282
Mubashwer Salman Khurshid <mskh@student.unimelb.edu.au>
2014-08-26 23:18:48 +1000

    Update create_tables.sql
    corrected syntax error and made a small name change of two attributes [my 4 tables]

--- create_tables.sql
+++ create_tables.sql
@@ -1,54 +1,55 @@
 CREATE TABLE Game(
-    GameID                  smallint       auto_increment, 
-    Genre                   varchar(50),
-    Review                  text,
-    StarRating              smallint,
-    ClassificationRating    varchar(5),
-    PlatformNotes           text,
-    PromotionLink           varchar(50),
-    Cost                    decimal(5,2),      
+    GameID                  SMALLINT       AUTO_INCREMENT, 
+    Genre                   VARCHAR(50),
+    Review                  TEXT,
+    StarRating              SMALLINT,
+    ClassificationRating    VARCHAR(5),
+    PlatformNotes           TEXT,
+    PromotionLink           VARCHAR(50),
+    Cost                    DECIMAL(5,2),      
     PRIMARY KEY(GameID)
 ) ENGINE=InnoDB;
     
-CREATE TABLE Video (
-    VideoID         smallint       auto_increment,
-    URL             varchar(50)    NOT_NULL,
-    Price           decimal(5,2),
-    VideoType       varchar(45),
-    InstanceRunID   smallint       NOT_NULL,
-    GameID          smallint       NOT_NULL,
-    PRIMARY KEY(AccountID),
-    FOREIGN KEY (InstanceRunID) REFERENCES (InstanceRun)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE,
-    FOREIGN KEY (GameID) REFERENCES (Game)
-        ON DELETE RESTRICT
-        ON UPDATE CASCADE
-) ENGINE=InnoDB;
-
 CREATE TABLE InstanceRun (
-    InstanceRunID         smallint       auto_increment,
-    SupervisorID          smallint       NOT_NULL,
-    Name                  varchar(45),
-    RecordedTime          DATETIME,
-    CategoryName          varchar(50),
+    InstanceRunID           SMALLINT       AUTO_INCREMENT,
+    SupervisorID            SMALLINT       NOT NULL,
+    InstanceName            VARCHAR(45),   # changed name from Name (as Name is part of syntax)
+    RecordedTime            DATETIME,
+    CategoryName            VARCHAR(50),
     PRIMARY KEY(InstanceRunID),
-    FOREIGN KEY (SupervisorID) REFERENCES (Player)
+    FOREIGN KEY (SupervisorID) REFERENCES Player(SupervisorID)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 CREATE TABLE Achievement (
-    AchievementID         smallint       auto_increment,
-    InstanceRunID         smallint       NOT_NULL,
-    WhenAchieved          DATETIME
-    Name                  varchar(45),
-    RewardBody            varchar(45),
+    AchievementID           SMALLINT       AUTO_INCREMENT,
+    InstanceRunID           SMALLINT       NOT NULL,
+    WhenAchieved            DATETIME,
+    AchievementName         VARCHAR(45),   # changed name from Name (as Name is part of syntax)
+    RewardBody              VARCHAR(45),
     PRIMARY KEY(AchievementID, InstanceRunID),
-    FOREIGN KEY (InstanceRunID) REFERENCES (InstanceRun)
+    FOREIGN KEY (InstanceRunID) REFERENCES InstanceRun(InstanceRunID)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
 ) ENGINE=InnoDB;
+
+CREATE TABLE Video (
+    VideoID                 SMALLINT       AUTO_INCREMENT,
+    URL                     VARCHAR(50)    NOT NULL,
+    Price                   DECIMAL(5,2),
+    VideoType               VARCHAR(45),
+    InstanceRunID           SMALLINT       NOT NULL,
+    GameID                  SMALLINT       NOT NULL,
+    PRIMARY KEY(VideoID),
+    FOREIGN KEY (InstanceRunID) REFERENCES InstanceRun(InstanceRunID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE,
+    FOREIGN KEY (GameID) REFERENCES Game(GameID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
 
 CREATE TABLE Viewer(
 
