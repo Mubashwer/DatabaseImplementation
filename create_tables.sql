d0866ce7413a31704ca36b6b792126cfdfe700a0
gwicks56 <geordiewicks@gmail.com>
2014-08-26 14:33:46 +1000

    Update create_tables.sql
    Geordies Tables

--- create_tables.sql
+++ create_tables.sql
@@ -50,4 +50,70 @@ CREATE TABLE Achievement {
         ON UPDATE CASCADE
 } ENGINE=InnoDB;
 
+CREATE TABLE Viewer(
+
+	ViewerID				SMALLINT		auto_increment,
+	ViewerType				CHAR(1)			NOT NULL,
+	DateOfBirth 			DATE 			,
+	Email					VARCHAR(50)		,
+	
+	PRIMARY KEY(ViewerID)
+
+
+)
+
+
+CREATE TABLE ViewerAddress(
+
+	ViewerID				SMALLINT		NOT NULL,
+	AddressID				SMALLINT 		NOT NULL,
+	StartDate				DATE 			??????,
+	EndDate					DATE 			,
+
+	PRIMARY KEY(StartDate)
+	FOREIGN KEY(ViewerID)			REFERENCES Viewer(ViewerID)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE,
+
+	FOREIGN KEY(AddressID)			REFERENCES Address(AddressID)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE
+
+)
+
+CREATE TABLE PlayerAddress(
+
+	PlayerID				SMALLINT		NOT NULL,
+	AddressID 				SMALLINT 		NOT NULL,
+	StartDate 				DATE 			??????,
+	EndDate 				DATE 			,
+
+	PRIMARY KEY(StartDate)
+	FOREIGN KEY(AddressID)			REFERENCES Address(AddressID)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE,
+	FOREIGN KEY(PlayerID)			REFERENCES Player(PlayerID)
+		ON DELETE RESTRICT
+		ON UPDATE CASCADE
+	)
+
+CREATE TABLE Address(
+
+	AddressID				SMALLINT		auto_increment,
+	StreetNumber			SMALLINT 		NOT NULL,
+	StreetNumberSuffix		VARCHAR(20)		,
+	StreetName				VARCHAR(50)		NOT NULL,
+	StreetType				VARCHAR(20)		NOT NULL,
+	AddressType 			VARCHAR(20)		,
+	AddressTypeIdentifier	VARCHAR(20)		,
+	MinorMunicipality		VARCHAR(50)		,
+	MajorMunicipality		VARCHAR(50)		NOT NULL,
+	GoverningDistrict		VARCHAR(50)		NOT NULL,
+	PostalArea				VARCHAR(4)		NOT NULL, ? varchar 4???? No US players???
+	Country					VARCHAR(50)		NOT NULL,
+
+	PRIMARY KEY(AddressID)
+)
+
+
 
