a95b24b820aa662f7c273f7621a5b063ddd3adb9
Mubashwer Salman Khurshid <mskh@student.unimelb.edu.au>
2014-10-05 00:45:46 +1000

    Update videos.py

--- videos.py
+++ videos.py
@@ -1,9 +1,9 @@
 # The libraries we'll need
-import sys, cgi, redirect, session
+import sys, cgi, redirect, session, MySQLdb
 
 # Get the session and check if logged in
 sess = session.Session(expires=60*20, cookie_path='/')
-loggedIn = sess.data.get('loggedIn')
+#loggedIn = sess.data.get('loggedIn') #anybody can access this page
 form = cgi.FieldStorage()
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -16,15 +16,23 @@ print """
 <meta name="keywords" content="" />
 <meta name="description" content="" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>WWAG Videos</title>
+<title>WWAG Video Search</title>
 <link href="css/login.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
 """
+
+print """
+<div class="main">
+    <hi><a href="home.py">HOME</a></h1>
+    <hi><a href="home.py">ABOUT US</a></h1>
+</div>
+"""
+
 print """
 <div class="search_form">
 <h2 class="header">VIDEOS</h2>
-<form action="videos.py" method="post">
+<form action="videos_search.py" method="post">
     <fieldset id="search">
         <legend>Video Search</legend>
         
@@ -32,6 +40,11 @@ print """
         <div class="textbox">
             <label for="video_id">Video ID:</label>
             <input name="video_id" id="video_id" type="text" />
+        </div>
+
+        <div class="textbox">
+            <label for="video_name">Video Name:</label>
+            <input name="video_name" id="video_name" type="text" />
         </div>
         
          <div class="textbox">
@@ -58,12 +71,12 @@ print """
             <label for="price">Price:</label>
             <input name="price" id="price" type="text" />
         </div>
-        
-        <div class="radio">
-            <input type="radio" name="video_type" id="premium" value="P"><span>Premium</span>    
-            <input type="radio" name="video_type" id="crowdfunding" value="C"><span>CrowdFunding</span>
+
+        <div class="textbox">
+            <label for="video_name">Video Type:</label>
+            <input name="video_type" id="video_type" type="text" />
         </div>
-    
+
     </fieldset>
     <div id="buttons" class="button_select">
         <input type="reset" value="Reset" />
@@ -74,8 +87,8 @@ print """
 """
 #TODO: not using radio buttons yet
 #Create a dictionary to map names used in HTML form to their corresponding MySQL database attribute names
-form_names = ['video_id', 'game_id', 'game_name', 'instance_run_id', 'instance_run_name', 'price']
-sql_col_names = ['VideoID', 'GameID', 'GameName', 'InstanceRunID', 'InstanceName', 'Price']
+form_names = ['video_id', 'video_name', 'game_id', 'game_name', 'instance_run_id', 'instance_run_name', 'price', 'video_type']
+sql_col_names = ['VideoID', 'VideoName', 'GameID', 'GameName', 'InstanceRunID', 'InstanceName', 'Price', 'VideoType']
 form_to_sql = dict(zip(form_names, sql_col_names))
     
 form_data = {}
@@ -83,8 +96,8 @@ for name in form_names:
     form_data[name] = form.getvalue(name)
         
 #TODO: connect to the correct database
-#db = MySQLdb.connect("info20003db.eng.unimelb.edu.au", "info20003", "cisds", "info20003", 3306)
-#cur = db.cursor()
+db = MySQLdb.connect("info20003db.eng.unimelb.edu.au", "info20003g29", "enigma29", "info20003g29", 3306)
+cursor = db.cursor()
               
 #The query will be made up of several elements, starting select
 select_elem = 'SELECT'
@@ -92,7 +105,7 @@ select_elem = 'SELECT'
 # These are the columns we wish to select from each table
 # select_cols[some_table] = [list, of, columns, from, some_table]
 select_cols = {}
-select_cols['Video'] = ['VideoID', 'URL', 'Price', 'VideoType']
+select_cols['Video'] = ['VideoID', 'VideoName', 'Price', 'VideoType']
 select_cols['Game'] = ['GameID', 'GameName']
 select_cols['InstanceRun'] = ['InstanceRunID', 'InstanceName']
 
@@ -117,10 +130,11 @@ where_elem = 'WHERE'
 #TODO add where clauses to conditions_s (based on form data, using like '%term%'
 # convert term to lower case before query? any other parsing?
 conditions = []
+values = []
 for (form_name, val) in form_data.items():
     if val:
         conditions.append(form_to_sql[form_name] + " LIKE '%" + val + "%'") 
-
+        values.append(val)
 conditions_elem = ' AND '.join(conditions)
 
 query_elems = [select_elem, columns_elem, from_elem, tables_elem]
@@ -128,15 +142,26 @@ if conditions:
     query_elems.append(where_elem)
     query_elems.append(conditions_elem)
     
-# Form query by joining it's elements, seperated by spaces, and appending a semicolon
-query = ' '.join(query_elems) + ';'
+# Form query by joining it's elements, seperated by spaces, and appending a semicolon and execute it
+query = ' '.join(query_elems) + ' LIMIT {};'
+cursor.execute(query.format(10))
+rows = cursor.fetchall()
 
-print query
-#TODO: execute query and return results
+# print results in table
+print '<table>'
+print '<tr><th>GameID</th><th>GameName</th><th>VideoID</th><th>VideoName</th><th>Price</th><th>VideoType</th><th>InstanceRunID</th><th>InstanceRunName</th></tr>'
+for row in rows:
+    print '<tr>'
+    video_id = row[2];
+    for element in row:
+        print '<td><span class="link"><a href="video.py?VideoID={}">{}</a></span></td>'.format(video_id, element)
+    print '</tr>'
+print '</table>'    
+
 print """
 </body>
 </html>
 """
 
 # Tidy up and free resources
-sess.close()​​​
+sess.close()
