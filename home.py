00ed2a4b17f674708cb22b28e773772a3c03d058
gwicks56 <geordiewicks@gmail.com>
2014-10-10 12:42:37 +1100

    Update home.py
    added extra headers

--- home.py
+++ home.py
@@ -35,24 +35,44 @@ print """
 """
 
 # Main HTML content, starting with header and main menu
-print """
-    <div id="header">
-	<div id="navbar">
-	<ul>
-            <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
-            <li><a href="Aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
-            <li><a href="logout.py" style="text-decoration:none;color:#fff">Log Out</a></li>
-            <li><a href="video_search.py" style="text-decoration:none;color:#fff">Videos</a>
-              	<ul>
-			<li><a href="#" style="text-decoration:none;color:#fff">Video 1</a></li>
-			<li><a href="#" style="text-decoration:none;color:#fff">Video 2</a></li>
-                	<li><a href="#" style="text-decoration:none;color:#fff">Video 3</a></li>
-		</ul>
-          	</li>
+if (not loggedIn or not userType == 'N'):
+   
+    print """
+<div id="header">
+    <div id="navbar">
+    <ul>
+        <li><a href="login.py" style="text-decoration:none;color:#fff">Log In</a></li>
+        <li><a href="Aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
+        
+        <li><a href="video_search.py" style="text-decoration:none;color:#fff">Videos</a></li>
+        <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
               
-	</ul>
-	</div>
+    </ul>
+    </div>
 </div>
+"""
+
+
+else:
+    
+    print """
+<div id="header">
+            <div id="navbar">
+                <ul>
+             <li><a href="logout.py" style="text-decoration:none;color:#fff">Log Out</a></li>
+            <li><a href="Aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
+            <li><a href="players.py" style="text-decoration:none;color:#fff">Players</a></li>
+            <li><a href="games.py" style="text-decoration:none;color:#fff">Games</a></li>
+            <li><a href="instance.py" style="text-decoration:none;color:#fff">Instance Runs</a></li>
+            <li><a href="achievements.py" style="text-decoration:none;color:#fff">Achievements</a></li>
+            <li><a href="Viewers.py" style="text-decoration:none;color:#fff">Viewers</a></li>
+            <li><a href="video_search.py" style="text-decoration:none;color:#fff">Videos</a></li>
+            <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
+                </ul>
+            </div>
+            
+  </div>
+"""
   
 <body>
   <p>
