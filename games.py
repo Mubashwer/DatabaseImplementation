a0b31f675708a27847479b35a082b03b27848d77
Mubashwer Salman Khurshid <mskh@student.unimelb.edu.au>
2014-10-10 23:09:46 +1100

    Update games.py

--- games.py
+++ games.py
@@ -15,7 +15,7 @@ form = cgi.FieldStorage()
 
 # ---------------------------------------------------------------------------------------------------------------------
 # Only logged in users who are players can access this page
-if 0 == 1 and (not loggedIn or not userType == 'S'):
+if (not loggedIn or not userType == 'S'):
     # redirect to home page
     print """\
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -39,16 +39,28 @@ print """
 <meta name="description" content="" />
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title>WWAG Players</title>
-<link href="css/user_details.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="css/video_modify.css" rel="stylesheet" type="text/css" media="screen" />
 </head>
 <body>
 """
 
 print """
-<div class="main">
-    <hi><a href="home.py">HOME</a></h1>
-    <hi><a href="home.py">ABOUT US</a></h1>
-</div>
+<div id="header">
+            <div id="navbar">
+                <ul>
+            <li><a href="do_logout.py" style="text-decoration:none;color:#fff">Log Out</a></li>
+            <li><a href="aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
+            <li><a href="players.py" style="text-decoration:none;color:#fff">Players</a></li>
+            <li><a href="games.py" style="text-decoration:none;color:#fff">Games</a></li>
+            <li><a href="instance_runs.py" style="text-decoration:none;color:#fff">Instance Runs</a></li>
+            <li><a href="achievements.py" style="text-decoration:none;color:#fff">Achievements</a></li>
+            <li><a href="viewers.py" style="text-decoration:none;color:#fff">Viewers</a></li>
+            <li><a href="videos_modify.py" style="text-decoration:none;color:#fff">Videos</a></li>
+            <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
+                </ul>
+            </div>
+            
+  </div>
 """
  
 ####### LOAD FORM DATA ##########################################################################
@@ -247,3 +259,4 @@ print """
 cursor.close()
 db.close()
 sess.close()                                                                                        
+
