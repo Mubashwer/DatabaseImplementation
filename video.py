53f5639c1997e3bf34001027cc095743ab8c3713
jheffernan <jheffernan@student.unimelb.edu.au>
2014-10-13 19:32:48 +1100

    Now prints Player info (properly)
    Removed problematic "get_all" helper function.
    Debugged MySQL natural joins to find that SupervisorID for Player and for InstanceRun were interacting and causing problems with q1, replaced these with inner joins.

--- video.py
+++ video.py
@@ -11,11 +11,6 @@ form = cgi.FieldStorage()
 def rows_from_res(res):
     return res.fetch_row(maxrows = 0, how = 1)
     #Get all rows, with each row as a dictionary with column names as keys and values as values
-
-def get_all(col_name = None, query_index = None):
-    [row[col_name] for row in all_rows[query_index]]
-    #Get the value in that column, for each row in the rows returned by the query with index
-    # query_index
     
 def uniq(l):
     return sorted(list(set(l)))#Cast to set, then back to remove dups
@@ -45,38 +40,31 @@ def wrap_table(text):
 db = MySQLdb.connect("info20003db.eng.unimelb.edu.au", "info20003g29", "enigma29", "info20003g29", 3306)
 
 video_id = form.getvalue("video_id")
-
+#If video_id is not set: redirect to videos_search
 if not video_id:
     print "Status: 307 Temporary Redirect"
-    print "Location: videos_search.py"
-    
+    print "Location: videos_search.py" 
+
 q0 = "SELECT * FROM Video NATURAL JOIN InstanceRun WHERE VideoID = '{}'".format(video_id)
 db.query(q0)
 res0 = db.store_result()
 rows0 = rows_from_res(res0)
 instance_id = rows0[0]["InstanceRunID"]
 
-#TESTING
-"""
-q1 = "SELECT * FROM InstanceRun NATURAL JOIN InstancePlayer NATURAL JOIN Player WHERE \
-InstanceRunID = '{}'".format(instance_id)
-"""
-q1 = "SELECT * FROM InstanceRun WHERE \
-InstanceRunID = '{}'".format(instance_id)
+q1 = "select * from Player inner join InstanceRun inner join InstancePlayer \
+on InstanceRun.InstanceRunID = InstancePlayer.InstanceRunID \
+AND Player.PlayerID = InstancePlayer.PlayerID \
+where InstanceRun.InstanceRunID = {}".format(instance_id)
+
 q2 = "SELECT * FROM Video NATURAL JOIN Game WHERE VideoID = '{}'".format(video_id)
+
 q3 = "SELECT * FROM Equipment NATURAL JOIN InstanceEquipment \
 NATURAL JOIN InstanceRun WHERE InstanceRunId = '{}'".format(instance_id)
 
-queries = [q1,q2,q3]# Get the session and check if logged in
-#Corresponding to [video/instance, player, game, equipment]
+queries = [q1,q2,q3]
+#Corresponding to [ player, game, equipment]
 results = [res0]
 all_rows = [rows0]
-
-#TESTING
-"""
-f="failed"
-fd = {"VideoName":"failed"}
-"""
 
 for query in queries:
     db.query(query)
@@ -85,7 +73,6 @@ for query in queries:
     all_rows.append(rows_from_res(result))  
 
 """
-
 Data returned by any query in list queries can now be accessed using it's index (e.g. 1 for q1)
 The general format is all_rows[query_index][row_index]["ColumnName"]
 Examples:
@@ -132,9 +119,11 @@ print """
     </div>
 </div>
 """
+
+
 #TODO: add order button, with possible access code input,
 #conditional on viewer type
-################################################################################
+
 #Print Video info
 video_info = []
 
@@ -152,51 +141,53 @@ for info in video_info:
 video_html = wrap_div(video_html, div_id="video_info")
 
 print(video_html)
-################################################################################
+
 #Print Player info
-#TESTING
-"""
-players = ["jaye","geordie","mubashwer","kendra"]
-"""
-uniq(get_all(col_name = "UserName", query_index = 1))
-  
-player_html = wrap_tr(wrap_td("Participating Players:"))
+
+player_html = wrap_tr(wrap_td("Participating Players:"))    
+
+players = uniq([row["UserName"] for row in all_rows[1]])
+
 for player in players:
-    #TODO wrap players in <a>s to link to player info page
+#TODO wrap players in <a>s to link to player info page
     player_html += wrap_tr(wrap_td(player))
+       
 player_html = wrap_div(wrap_table(player_html), 'player_info')
 
 print(player_html)
-################################################################################
+
 #Print Game info
 #TODO add link  to game info page
 game_name = all_rows[2][0]["GameName"]
 print(wrap_div(wrap_p("Game: " + game_name)))
-################################################################################
+
 #Print equipment info
-equipments = get_all(col_name = "ModelAndMake", query_index = 3)
+
+equipments = uniq([row["ModelAndMake"] for row in all_rows[3]])
+
 equipment_html = wrap_tr(wrap(td("Equipment Used:")))
 for equipment in equipments:
     #TODO wrap equipments in <a>s to link to equipment info page
     equipment_html += wrap_tr(wrap_td(equipment))
 equipment_html = wrap_div(wrap_table(equipment_html), div_id = 'equipment_info')
 print(equipment_html)
-################################################################################
+
 #Print InstanceRun info
+
 ir_info = []
 
 ir_info.append("Instance Run Details:")
-ir_info.append("Name: " + all_rows[1][0]["InstanceName"])
-ir_info.append("Recorded time: " + str(all_rows[1][0]["RecordedTime"]))
-ir_info.append("Category name: " + all_rows[1][0]["CategoryName"])
+ir_info.append("Name: " + all_rows[0][0]["InstanceName"])
+ir_info.append("Recorded time: " + str(all_rows[0][0]["RecordedTime"]))
+ir_info.append("Category name: " + all_rows[0][0]["CategoryName"])
 
 ir_html =""
 for line in ir_info:
     ir_html += wrap_p(line)
 ir_html = wrap_div(ir_html, div_id = "instance_run_info")
-################################################################################
 
 print(ir_html)
+
 print """
 </body>
 </html>
