8410b382b19b05d500c5688444cd46da27d39305
jheffernan <jheffernan@student.unimelb.edu.au>
2014-10-15 00:33:24 +1100

    Create common.py
    This is a module containing functions for commonly used code snippets throughout our various pages.  It can be used simply by adding 'import common' at the top of your file then calling functions with a prefix of 'common.'.  Typical usage of current functions may look like:
    ###
    import sess, ..., MySQLdb, common
    
    do some things
    
    print common.make_head(title = 'WWAG Window Title')
    print common.make_navbar()
    
    do some things
    
    print all of your body html
    
    print common.end_html
    ###

---
+++ common.py
@@ -0,0 +1,44 @@
+def make_head(title = 'WWAG', head_inject = ''):
+    """Constructs and returns the html header, taking as title and head_inject as inputs.
+    title will be the window title.  head_inject will be inserted directly after the 
+    opening <head> tag.  It could, for example, contain a <meta> tag used to implement
+    a redirect.  The string returned also initialises the <html> and <body> branches.
+    All text following will be part of the body."""
+    
+    return"""
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    {}
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>{}</title>
+    <link href="css/video.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+    """.format(head_inject, title)
+
+def make_navbar():
+    """Constructs and returns html string for the basic navbar used by most (all?) 
+    pages on the site"""
+    
+    return  """
+    <div id="header">
+        <div id="navbar">
+        <ul>
+            <li><a href="login.py" style="text-decoration:none;color:#fff">Log In</a></li>
+            <li><a href="aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>    
+            <li><a href="videos_search.py" style="text-decoration:none;color:#fff">Videos</a></li>
+            <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
+        </ul>
+        </div>
+    </div>
+    """
+
+def end_html():
+    """Generates string of closing </body> and </html> tags to finish an html document."""
+    
+    return """
+</body>
+</html>
+    """
