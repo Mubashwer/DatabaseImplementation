29081955673b97c7849f5e49a903670a604cd351
jheffernan <jheffernan@student.unimelb.edu.au>
2014-10-15 17:10:11 +1100

    3 Major Improvements
    - Protected against SQL injection by switching db.query()s to cursor.execute()s
    - Added string substitution to keep last search values in the form while displaying their results
    - Searches for game-name, video-name and instance-run-name are now case-insensitive
    
    Note: reset button does not currently work

--- videos_search.py
+++ videos_search.py
@@ -1,6 +1,6 @@
 # The libraries we'll need
 import sys, cgi, redirect, session, MySQLdb
-
+from common import *
 # Get the session and check if logged in
 sess = session.Session(expires=60*20, cookie_path='/')
 #loggedIn = sess.data.get('loggedIn') #anybody can access this page
@@ -10,91 +10,12 @@ form = cgi.FieldStorage()
 # send session cookie
 print "%s\nContent-Type: text/html\n" % (sess.cookie)
 
-print """
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>WWAG Video Search</title>
-<link href="css/video_search.css" rel="stylesheet" type="text/css" media="screen" />
-</head>
-<body>
-"""
+print make_head(title = "WWAG Video Search", css_file = 'video_search.css')
 
-print """
-<div id="header">
-    <div id="navbar">
-    <ul>
-        <li><a href="login.py" style="text-decoration:none;color:#fff">Log In</a></li>
-        <li><a href="aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
-        
-        <li><a href="videos_search.py" style="text-decoration:none;color:#fff">Videos</a></li>
-        <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
-              
-    </ul>
-    </div>
-</div>
-"""
+print make_navbar()
 
-print """
-<div class="search_form">
-<h2 class="header">VIDEOS</h2>
-<form action="videos_search.py" method="post">
-    <fieldset id="search">
-        <legend>Video Search</legend>
-        
 
-        <div class="textbox">
-            <label for="video_id">Video ID:</label>
-            <input name="video_id" id="video_id" type="text" />
-        </div>
 
-        <div class="textbox">
-            <label for="video_name">Video Name:</label>
-            <input name="video_name" id="video_name" type="text" />
-        </div>
-        
-         <div class="textbox">
-            <label for="game_id">Game ID:</label>
-            <input name="game_id" id="game_id" type="text" />
-        </div>
-
-        <div class="textbox">
-            <label for="game_name">Game Name:</label>
-            <input name="game_name" id="game_name" type="text" />
-        </div>
-
-        <div class="textbox">
-            <label for="instance_run_id">InstanceRun ID:</label>
-            <input name="instance_run_id" id="instance_run_id" type="text" />
-        </div>
-
-        <div class="textbox">
-            <label for="instance_run_name">InstanceRun Name:</label>
-            <input name="instance_run_name" id="instance_run_name" type="text" />
-        </div>
-
-         <div class="textbox">
-            <label for="price">Price:</label>
-            <input name="price" id="price" type="text" />
-        </div>
-
-        <div class="textbox">
-            <label for="video_name">Video Type:</label>
-            <input name="video_type" id="video_type" type="text" />
-        </div>
-
-    </fieldset>
-    <div id="buttons" class="button_select">
-        <input type="reset" value="Reset" />
-        <input type="submit" value="Submit" />
-    </div>
-</form>
-</div>
-"""
-#TODO: not using radio buttons yet
-#Create a dictionary to map names used in HTML form to their corresponding MySQL database attribute names
 form_names = ['video_id', 'video_name', 'game_id', 'game_name', 'instance_run_id', 'instance_run_name', 'price', 'video_type']
 sql_col_names = ['VideoID', 'VideoName', 'GameID', 'GameName', 'InstanceRunID', 'InstanceName', 'Price', 'VideoType']
 form_to_sql = dict(zip(form_names, sql_col_names))
@@ -102,8 +23,62 @@ form_to_sql = dict(zip(form_names, sql_col_names))
 form_data = {}
 for name in form_names:
     form_data[name] = form.getvalue(name)
+
+print """
+<div class="search_form">
+<h2 class="header">VIDEOS</h2>
+<form action="videos_search.py" method="post">
+    <fieldset id="search">
+        <legend>Video Search</legend>
+        <div class="textbox">
+            <label for="video_id">Video ID:</label>
+            <input name="video_id" id="video_id" type="text" value="{}"/>
+        </div>
+
+        <div class="textbox">
+            <label for="video_name">Video Name:</label>
+            <input name="video_name" id="video_name" type="text" value="{}"/>
+        </div>
         
-#TODO: connect to the correct database
+         <div class="textbox">
+            <label for="game_id">Game ID:</label>
+            <input name="game_id" id="game_id" type="text" value="{}"/>
+        </div>
+
+        <div class="textbox">
+            <label for="game_name">Game Name:</label>
+            <input name="game_name" id="game_name" type="text" value="{}"/>
+        </div>
+
+        <div class="textbox">
+            <label for="instance_run_id">InstanceRun ID:</label>
+            <input name="instance_run_id" id="instance_run_id" type="text" value="{}"/>
+        </div>
+
+        <div class="textbox">
+            <label for="instance_run_name">InstanceRun Name:</label>
+            <input name="instance_run_name" id="instance_run_name" type="text" value="{}"/>
+        </div>
+
+         <div class="textbox">
+            <label for="price">Price:</label>
+            <input name="price" id="price" type="text" value="{}"/>
+        </div>
+
+        <div class="textbox">
+            <label for="video_name">Video Type:</label>
+            <input name="video_type" id="video_type" type="text" value="{}"/>
+        </div>
+
+    </fieldset>
+    <div id="buttons" class="button_select">
+            <input type="reset" value="Reset" />       
+            <input type="submit" value="Submit" />
+    </div>
+</form>
+</div>
+""".format(*[form_data[name] if form_data[name] is not None else '' for name in form_names])
+
 db = MySQLdb.connect("info20003db.eng.unimelb.edu.au", "info20003g29", "enigma29", "info20003g29", 3306)
 cursor = db.cursor()
               
@@ -126,7 +101,7 @@ for columns in select_cols.values():
     for column in columns:
         all_columns.append(column)
 
-columns_elem = ','.join(all_columns)
+columns_elem = 'LOWER(VideoName),LOWER(GameName),LOWER(InstanceName),' + ','.join(all_columns)
 
 from_elem = 'FROM'
 
@@ -135,13 +110,17 @@ tables_elem = ' NATURAL JOIN '.join(tables)
 
 where_elem = 'WHERE'
 
-#TODO add where clauses to conditions_s (based on form data, using like '%term%'
-# convert term to lower case before query? any other parsing?
+subs = []#list of string substitution to be made into the query
 conditions = []
 values = []
 for (form_name, val) in form_data.items():
     if val:
-        conditions.append(form_to_sql[form_name] + " LIKE '%" + val + "%'") 
+        if form_name in ['video_name', 'game_name', 'instance_run_name']:
+                conditions.append('LOWER({}) LIKE %s'.format(form_to_sql[form_name]))
+                subs.append("%" + val.lower() + "%")
+        else:
+            conditions.append(form_to_sql[form_name] + " LIKE %s")
+            subs.append("%" + val + "%")
         values.append(val)
 conditions_elem = ' AND '.join(conditions)
 
@@ -149,10 +128,10 @@ query_elems = [select_elem, columns_elem, from_elem, tables_elem]
 if conditions:
     query_elems.append(where_elem)
     query_elems.append(conditions_elem)
-    
+limit_elem =  'LIMIT 40'   
 # Form query by joining it's elements, seperated by spaces, and appending a semicolon and execute it
-query = ' '.join(query_elems) + ' LIMIT {};'
-cursor.execute(query.format(10))
+query = ' '.join(query_elems) + ';'
+cursor.execute(query, tuple(subs))
 rows = cursor.fetchall()
 
 # print results in table
@@ -160,16 +139,13 @@ print '<table>'
 print '<tr><th>GameID</th><th>GameName</th><th>VideoID</th><th>VideoName</th><th>Price</th><th>VideoType</th><th>InstanceRunID</th><th>InstanceRunName</th></tr>'
 for row in rows:
     print '<tr>'
-    video_id = row[2];
-    for element in row:
+    video_id = row[5];
+    for element in row[3:]:#Skip over LOWER(whatever)s when printing
         print '<td><span class="link"><a href="video.py?video_id={}">{}</a></span></td>'.format(video_id, element)
     print '</tr>'
 print '</table>'    
 
-print """
-</body>
-</html>
-"""
-
+print end_html
+                
 # Tidy up and free resources
 sess.close()
