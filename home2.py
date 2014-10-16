5c1e11efe69949964e68f8df3031450fcdd8a3d2
gwicks56 <geordiewicks@gmail.com>
2014-10-16 16:02:34 +1100

    Create home2.py

---
+++ home2.py
@@ -0,0 +1,99 @@
+# The libraries we'll need
+import sys, cgi, redirect, session
+
+# Get the session and check if logged in
+sess = session.Session(expires=60*20, cookie_path='/')
+loggedIn = sess.data.get('loggedIn')
+userType = sess.data.get('userType')
+
+# send session cookie
+print "%s\nContent-Type: text/html\n" % (sess.cookie)
+
+# get form data
+form = cgi.FieldStorage()
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+href = {None: ('Log In', 'login.py'), 0: ('Log In', 'login.py'), 1: ('Log Out', 'do_logout.py')}
+
+print """
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>WWAG Home</title>
+<link href="css/home.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+"""
+if (not loggedIn):
+    print """
+<div id="header">
+    <div id="navbar">
+    <ul>
+        <li><a href=""login.py" style="text-decoration:none;color:#fff">Log In</a></li>
+        <li><a href="aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
+        
+        <li><a href="videos_search.py" style="text-decoration:none;color:#fff">Videos</a></li>
+        <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
+              
+    </ul>
+    </div>
+</div>
+
+""".format(href[loggedIn][1], href[loggedIn][0])
+
+else if(loggedIn && userType != 'S')
+   print """
+<div id="header">
+    <div id="navbar">
+    <ul>
+        <li><a href=""logout.py" style="text-decoration:none;color:#fff">LogOut </a></li>
+        <li><a href="aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
+        
+        <li><a href="videos_search.py" style="text-decoration:none;color:#fff">Videos</a></li>
+        <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
+              
+    </ul>
+    </div>
+</div>
+
+else:
+    
+    print """
+<div id="header">
+            <div id="navbar">
+                <ul>
+            <li><a href="do_logout.py" style="text-decoration:none;color:#fff">Log Out</a></li>
+            <li><a href="aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
+            <li><a href="players.py" style="text-decoration:none;color:#fff">Players</a></li>
+            <li><a href="games.py" style="text-decoration:none;color:#fff">Games</a></li>
+            <li><a href="instance_runs.py" style="text-decoration:none;color:#fff">Instance Runs</a></li>
+            <li><a href="achievements.py" style="text-decoration:none;color:#fff">Achievements</a></li>
+            <li><a href="viewers.py" style="text-decoration:none;color:#fff">Viewers</a></li>
+            <li><a href="videos_modify.py" style="text-decoration:none;color:#fff">Videos</a></li>
+            <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
+                </ul>
+            </div>
+            
+  </div>
+"""
+
+print """
+  
+<body>
+  <p>
+    <div id="background">
+      <p id="text"> WWAG </p>
+      <p id="text2"> The Wil Wheaton Appreciation Guild </p>
+  </div>
+"""
+
+print """
+</body>
+</html>
+"""
+
+# Tidy up and free resources
+sess.close()
