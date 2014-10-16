9bedf19ccbe41f6b977051b3457b1583d62c258f
Mubashwer Salman Khurshid <mskh@student.unimelb.edu.au>
2014-10-16 20:46:21 +1100

    Update videos_modify.py

--- videos_modify.py
+++ videos_modify.py
@@ -1,277 +1,152 @@
 # The libraries we'll need
-import sys, cgi, redirect, session, MySQLdb
+import sys, cgi, redirect, session, MySQLdb, warnings, sql, html
+from xml.sax.saxutils import *
+warnings.filterwarnings('error', category=MySQLdb.Warning)
 
 # Get the session and check if logged in
 sess = session.Session(expires=60*20, cookie_path='/')
 loggedIn = sess.data.get('loggedIn')
 userType = sess.data.get('userType')
-form = cgi.FieldStorage()
 
-# ---------------------------------------------------------------------------------------------------------------------
 # send session cookie
 print "%s\nContent-Type: text/html\n" % (sess.cookie)
 
+# get form data
+form = cgi.FieldStorage()
+# additional entity to replace in escape function
+entities = {'"': '&quot;'} 
 # ---------------------------------------------------------------------------------------------------------------------
 # Only logged in users who are players can access this page
-if not loggedIn or not userType == 'S':
+if (not loggedIn or not userType == 'S'):
     # redirect to home page
-    print """\
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <meta http-equiv="refresh" content="0;url=%s">
-    </head>
-    <body>
-    </body>
-    """ % redirect.getQualifiedURL("/~mskh/dbsys/dbs2014sm2group29/home.py")
+    print html.do_redirect("home.py")
     sess.close()   
     sys.exit(0)
 
 # ---------------------------------------------------------------------------------------------------------------------
     
-print """
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta name="keywords" content="" />
-<meta name="description" content="" />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<title>WWAG Instance Runs</title>
-<link href="css/video_modify.css" rel="stylesheet" type="text/css" media="screen" />
-</head>
-<body>
-"""
+print html.make_head("video_modify.css", title="WWAG Video")
 
-
-print """
-<div id="header">
-            <div id="navbar">
-                <ul>
-            <li><a href="do_logout.py" style="text-decoration:none;color:#fff">Log Out</a></li>
-            <li><a href="aboutme.py" style="text-decoration:none;color:#fff">About Us</a></li>
-            <li><a href="players.py" style="text-decoration:none;color:#fff">Players</a></li>
-            <li><a href="games.py" style="text-decoration:none;color:#fff">Games</a></li>
-            <li><a href="instance_runs.py" style="text-decoration:none;color:#fff">Instance Runs</a></li>
-            <li><a href="achievements.py" style="text-decoration:none;color:#fff">Achievements</a></li>
-            <li><a href="viewers.py" style="text-decoration:none;color:#fff">Viewers</a></li>
-            <li><a href="videos_modify.py" style="text-decoration:none;color:#fff">Videos</a></li>
-            <li><a href="home.py" style="text-decoration:none;color:#fff">Home</a></li>
-                </ul>
-            </div>
-            
-  </div>
-"""
-
+print html.make_navbar(loggedIn, userType)
 
 print """
 <div class="search_form">
 <h2 class="header">VIDEOS</h2>
-<form action="videos_modify.py" method="post">
+<form id = "myForm" action="videos_modify.py" method="post">
     <fieldset id="search">
         <legend>Video Search</legend>
         
 
         <div class="textbox">
-            <label for="video_id">Video ID:</label>
-            <input name="video_id" id="video_id" type="text" />
+            <label for="VideoID">Video ID:</label>
+            <input name="VideoID" id="VideoID" type="text" />
         </div>
 
         <div class="textbox">
-            <label for="video_name">Video Name:</label>
-            <input name="video_name" id="video_name" type="text" />
+            <label for="VideoName">Video Name:</label>
+            <input name="VideoName" id="VideoName" type="text" />
         </div>
         
          <div class="textbox">
-            <label for="game_id">Game ID:</label>
-            <input name="game_id" id="game_id" type="text" />
+            <label for="GameID">Game ID:</label>
+            <input name="GameID" id="GameID" type="text" />
         </div>
 
         <div class="textbox">
-            <label for="game_name">Game Name:</label>
-            <input name="game_name" id="game_name" type="text" />
-        </div>
-
-        <div class="textbox">
-            <label for="instance_run_id">InstanceRun ID:</label>
-            <input name="instance_run_id" id="instance_run_id" type="text" />
-        </div>
-
-        <div class="textbox">
-            <label for="instance_run_name">InstanceRun Name:</label>
-            <input name="instance_run_name" id="instance_run_name" type="text" />
+            <label for="InstanceRunID">InstanceRun ID:</label>
+            <input name="InstanceRunID" id="InstanceRunID" type="text" />
         </div>
 
          <div class="textbox">
-            <label for="price">Price:</label>
-            <input name="price" id="price" type="text" />
+            <label for="Price">Price:</label>
+            <input name="Price" id="Price" type="text" />
         </div>
 
         <div class="textbox">
-            <label for="video_type">Video Type:</label>
-            <input name="video_type" id="video_type" type="text" />
+            <label for="VideoType">Video Type:</label>
+            <input name="VideoType" id="VideoType" type="text" />
         </div>
 
         <div class="textbox">
-            <label for="video_url">URL:</label>
-            <input name="video_url" id="video_url" type="text" />
+            <label for="URL">URL:</label>
+            <input name="URL" id="URL" type="text" />
         </div>
 
     </fieldset>
     <div id="buttons" class="button_select">
         <input type="reset" value="Reset" />
         <input type="submit" name="submit" value="Insert" />
-        <input type="submit" name="submit" value="Search" />
+        <input type="submit" name="submit" value="Search" onClick="DoSubmit()" />
     </div>
 </form>
 </div>
 """
-#connect to the correct database
+
+
+####### CONNECT TO DATABASE AND LOAD FORM DATA ##########################################################################
+
 db = MySQLdb.connect("info20003db.eng.unimelb.edu.au", "info20003g29", "enigma29", "info20003g29", 3306)
 cursor = db.cursor()
-query = ''
+table = "Video"
+keys = ['VideoID', 'VideoName', 'URL', 'Price', 'VideoType', 'InstanceRunID', 'GameID']
+exact_keys = ['VideoID', 'Price', 'InstanceRunID', 'GameID']
+pk = ['VideoID']
+fields = dict.fromkeys(keys)
 
-#TODO: not using radio buttons yet
-#Create a dictionary to map names used in HTML form to their corresponding MySQL database attribute names
-form_names = ['video_id', 'video_name', 'game_id', 'game_name', 'instance_run_id', 'instance_run_name', 'price', 'video_type', 'video_url']
-sql_col_names = ['VideoID', 'VideoName', 'GameID', 'GameName', 'InstanceRunID', 'InstanceName', 'Price', 'VideoType', 'URL']
-form_to_sql = dict(zip(form_names, sql_col_names))
-
-form_data = {}
-for name in form_names:
-    form_data[name] = form.getvalue(name)
-    
-####### For INSERT AND UPDATE ################################################################################    
-#If fields with default values are left blank, then DEFAULT is used
-if form_data['video_name'] ==  None:
-    video_name = 'DEFAULT'
-else:
-    video_name = "'" + form_data['video_name'] + "'" 
-if form_data['price'] ==  None:
-    price = 'DEFAULT'
-else:
-    price = "'" + form_data['price'] + "'" 
-if form_data['video_type'] ==  None:
-    video_type = 'DEFAULT'
-else:
-    video_type = "'" + form_data['video_type'] + "'" 
-
-######## If INSERT button is pressed then ... ###############################################################
-if form.getvalue("submit") == "Insert":
-
-    #insert query is generated and there is an attempt to execute the query
-    query = '''INSERT INTO Video VALUES (DEFAULT, {}, '{}', {}, {}, '{}', '{}');'''.format(video_name, form_data['video_url'], price, video_type, form_data['instance_run_id'], form_data['game_id']);
-    try:   
-        cursor.execute(query)
-        db.commit()
-        print '<div class = "success">Insert Successful</div>'
-    except:
-        print '<div class = "error">Insert Error!. Please ensure all required fields are filled in proper format.</div>'
+for key in fields:
+    fields[key] = form.getvalue(key) 
 
 
-######## If DELETE button is pressed then ... ###############################################################        
+######## If INSERT button is pressed then ... ###########################################################################
+if form.getvalue("submit") == "Insert":    
+    print sql.insert(db, cursor, table, fields, keys)
+
+######## If DELETE button is pressed then ... ###########################################################################        
 if form.getvalue("submit") == "Delete":
-    query = "DELETE FROM Video WHERE VideoID = '{}';".format(form_data['video_id'])
-    try:   
-        cursor.execute(query)
-        db.commit()
-        print '<div class = "success">Delete Successful</div>'
-    except:
+    print sql.delete(db, cursor, table, fields, pk)
         
-        print '<div class = "error">Delete Error!. Please ensure all the fields which are filled matches with a record.</div>'
-        
-######## If UPDATE button is pressed then ... ###############################################################
+######## If UPDATE button is pressed then ... ############################################################################
 if form.getvalue("submit") == "Update":        
-    query = "UPDATE Video SET VideoName = {}, URL = '{}', Price = {}, VideoType = {}, GameID = '{}', InstanceRunID = '{}' WHERE VideoID = '{}';".format(video_name, form_data['video_url'], price, video_type,
-                                                                                                                                                        form_data['game_id'], form_data['instance_run_id'], form_data['video_id']);
-    try:   
-        cursor.execute(query)
-        db.commit()
-        print '<div class = "success">Update Successful</div>'
-    except:
-        
-        print '<div class = "error">Update Error!. Please ensure all required fields are filled in proper format.</div>'
-
-######## Generate Table of Data ... ###############################################################    
-#The query will be made up of several elements, starting select
-select_elem = 'SELECT'
-              
-# These are the columns we wish to select from each table
-# select_cols[some_table] = [list, of, columns, from, some_table]
-select_cols = {}
-select_cols['Video'] = ['VideoID', 'VideoName', 'Price', 'VideoType', 'URL']
-select_cols['Game'] = ['GameID', 'GameName']
-select_cols['InstanceRun'] = ['InstanceRunID', 'InstanceName']
-
-
-#Luckily the column names are unique (except for foreign keys)
-#We compile a list of names and then join them to form a string, with commas seperating
-all_columns = []
-
-for columns in select_cols.values():
-    for column in columns:
-        all_columns.append(column)
-
-columns_elem = ','.join(all_columns)
-
-from_elem = 'FROM'
-
-tables = select_cols.keys()
-tables_elem = ' NATURAL JOIN '.join(tables)
-
-where_elem = 'WHERE'
-
-#TODO add where clauses to conditions_s (based on form data, using like '%term%'
-# convert term to lower case before query? any other parsing?
-conditions = []
-values = []
-for (form_name, val) in form_data.items():
-    if val:
-        conditions.append(form_to_sql[form_name] + " LIKE '%" + val + "%'") 
-        values.append(val)
-conditions_elem = ' AND '.join(conditions)
-
-query_elems = [select_elem, columns_elem, from_elem, tables_elem]
-if conditions:
-    query_elems.append(where_elem)
-    query_elems.append(conditions_elem)
+    print sql.update(db, cursor, table, fields, keys, pk) 
     
-# Form query by joining it's elements, seperated by spaces, and appending a semicolon and execute it
-query = ' '.join(query_elems) + ' LIMIT {};'
-cursor.execute(query.format(10))
-rows = cursor.fetchall()
+####### GENERATE AND EXECUTE SEARCH QUERY  ################################################################################
+result =  sql.search(db, cursor, table, fields, keys, exact_keys, limit=10)
+rows = result[0];
+print result[1]; 
 
-# print results in table
-print '<table>'
-print '<tr><th>GameID</th><th>GameName</th><th>VideoID</th><th>VideoName</th><th>Price</th><th>VideoType</th><th>URL</th><th>InstanceRunID</th><th>InstanceRunName</th><th>Update</th><th>Delete</th></tr>'
-for row in rows:
-    print '<tr>'
-    print '<form action="videos_modify.py" method="post">'
-    video_id = row[2];
-    print '<input name="video_id" id="video_id" type="hidden" value = "{0}" />'.format(video_id)
-    
-    print '<td><span class="row_textbox"><input name="game_id" id="game_id" type="text" value = "{}" /></span></td>'.format(row[0])
-    print '<td><span class="link"><a href="video.py?VideoID={}">{}</a></span></td>'.format(video_id, row[1])
-    print '<td><span class="link"><a href="video.py?VideoID={0}">{0}</a></span></td>'.format(video_id)
-    print '<td><span class="row_textbox"><input name="video_name" id="video_name" type="text" value = "{}" /></span></td>'.format(row[3])
-    print '<td><span class="row_textbox"><input name="price" id="price" type="text" value = "{}" /></span></td>'.format(row[4])
-    print '<td><span class="row_textbox"><input name="video_type" id="video_type" type="text" value = "{}" /></span></td>'.format(row[5])
-    print '<td><span class="row_textbox"><input name="video_url" id="video_url" type="text" value = "{}" /></span></td>'.format(row[6])
-    print '<td><span class="row_textbox"><input name="instance_run_id" id="instance_run_id" type="text" value = "{}" /></span></td>'.format(row[7])
-    print '<td><span class="link"><a href="video.py?VideoID={}">{}</a></span></td>'.format(video_id, row[8])
-    
-    
-    print '<td><input type="submit" name="submit" value="Update" /></td><td><input type="submit" name="submit" value="Delete" /></td>'
-    print '</form>'
-    print '</tr>'
+####### DISPLAY RESULTS TABLE  #############################################################################################
+print '<table class="gridtable" align="center">'
 
+# Print column headers    
+print '<tr>'
+for key in keys:
+    print '<th>{}</th>'.format(key)
+print '<th>Update</th><th>Delete</th>'
+print '</tr>'
 
-    
+# Print each row of table    
+if rows != None: 
+    for row in rows:
+        print '<tr>'
+        print '<form action="videos_modify.py" method="post">'
+        i = 0
+        print '<td><input name="VideoID" id="VideoID" type="hidden" value = "{0}" /><a href="video.py?VideoID={0}">{0}</a></td>'.format(row[i])
+        i += 1
+        # Print each field of row as textbox
+        for key in keys[1:]:
+            if row[i] == None:
+                field = ''
+            else:
+                field = row[i]
+            print '<td><span class="row_textbox"><input name="{0}" id="{0}" type="text" value = "{1}" /></span></td>'.format(key, escape(str(field), entities))
+            i += 1
+        print '<td><input type="submit" name="submit" value="Update" /></td><td><input type="submit" name="submit" value="Delete" /></td>'
+        print '</form>'
+        print '</tr>'
+                                                                                             
 print '</table>'    
 
-print """
-</body>
-</html>
-"""
+print html.end_html
 
 # Tidy up and free resources
 sess.close()
