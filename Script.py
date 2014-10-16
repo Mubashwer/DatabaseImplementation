226ccdd53e2345365f8350aa7e0fa3b2746d4fcc
Mubashwer Salman Khurshid <mskh@student.unimelb.edu.au>
2014-10-16 14:18:03 +1100

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,14 +1,9 @@
 script = '''
-<script type="text/javascript">
-
     function DoSubmit(str){
         var elem = document.getElementById('myForm').elements;
         for(var i = 0; i < elem.length; i++) 
             if(elem[i].name != "submit" && elem[i].name != "")    
-                if(str == "Search")
-                    elem[i].value = EscapeSearch(elem[i].value);
-                else
-                    elem[i].value = Escape(elem[i].value);
+                elem[i].value = Escape(elem[i].value);
     
         myForm.submit();
         return true;
@@ -26,29 +21,10 @@ script = '''
                 case "\\\"":
                 case "\'":
                 case "\\\\":
-                    return "\\\\"+char;
                 case "%":
-                    return "%";
+                    return "\\\\"+char;
             }
        });
     };
-    
-    function EscapeSearch (str) {
-        return str.replace(/[\\0\\n\\r"'\\\\\\%]/g, function (char) {
-            switch (char) {
-                case "\\0":
-                    return "\\\\0";
-                case "\\n":
-                    return "\\\\n";
-                case "\\r":
-                    return "\\\\r";
-                case "\\\"":
-                case "\\\\":
-                case "\'":
-                case "%":
-                    return "\\\\"+char;
-            }
-        });
-    };
 
-</script>'''​
+'''
