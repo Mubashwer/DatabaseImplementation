7acf34e4afa698a2388356621dcb95d59c2c9673
Mubashwer Salman Khurshid <mskh@student.unimelb.edu.au>
2014-10-17 16:23:34 +1100

    Create load_data.sql

---
+++ load_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Game VALUES (1, 'MMORPG', DEFAULT, '4', 'PG', 'PC', 'http://dragonnest.nexon.net/', DEFAULT, 'Dragon Nest');
+INSERT INTO Game VALUES (2, 'Sports', DEFAULT, '5', 'PG', 'PC, PS3, PS4, Xbox 360, Xbox One', 'http://www.easports.com/fifa', DEFAULT, 'FIFA 15');
