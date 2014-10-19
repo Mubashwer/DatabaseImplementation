f619f613be446e5cba2da5d47a624ed782665cc8
gwicks56 <geordiewicks@gmail.com>
2014-10-19 19:54:51 +1100

    Update viewers.py
    fixed geordie

--- viewers.py
+++ viewers.py
@@ -1,3 +1,4 @@
+
 # The libraries we'll need
 import sys, cgi, redirect, session, MySQLdb, warnings, hashlib, uuid, html, sql
 from xml.sax.saxutils import *
@@ -27,11 +28,11 @@ function hide(ladiv){
 function details(){
     hide("crowd");
     hide("premium");
-    if(document.forms.myForm.ViewerType[0].checked==true)
+    if(document.forms.myForm.ViewerType.value == 'C')
         show("crowd");
-    else if(document.forms.myForm.ViewerType[1].checked==true)
+    else if(document.forms.myForm.ViewerType.value == 'P')
         show("premium");
-    else if(document.forms.myForm.ViewerType[2].checked==true) {
+    else if(document.forms.myForm.ViewerType.value == 'B') {
         show("crowd");
         show("premium");
     }    
@@ -50,7 +51,102 @@ if (not loggedIn or not userType == 'S'):
     sess.close()   
     sys.exit(0)
 
-# ---------------------------------------------------------------------------------------------------------------------
+####### CONNECT TO DATABASE AND LOAD FORM DATA ##########################################################################
+
+db = MySQLdb.connect("info20003db.eng.unimelb.edu.au", "info20003g29", "enigma29", "info20003g29", 3306)
+cursor = db.cursor()
+table = 'Viewer'
+keys = ['ViewerID', 'ViewerType', 'DateOfBirth','Email', 'UserName', 'HashedPassword', 'Salt']
+keys_c = ['ViewerID', 'FirstName', 'LastName', 'TotalAmountDonated']
+keys_p = ['ViewerID', 'RenewalDate']
+exact_keys = ['ViewerID', 'TotalAmountDonated']
+ignore_keys = ['ViewerType', 'DateOfBirth','Email', 'UserName', 'HashedPassword', 'Salt', 'FirstName', 'LastName', 'TotalAmountDonated', 'RenewalDate']
+message = ""
+
+fields = dict.fromkeys(keys + keys_c + keys_p + ['ViewerTypeOld'])
+
+for key in fields:
+    fields[key] = form.getvalue(key)
+
+######## If UPDATE button is pressed then ... ############################################################################
+ignore_update = [];
+if form.getvalue("submit") == "Update":        
+    fields['Salt'] = uuid.uuid4().hex
+    if fields["HashedPassword"] != None:
+        fields["HashedPassword"] = hashlib.sha512(fields['HashedPassword'] + fields['Salt']).hexdigest()
+    else:
+        ignore_update = ['HashedPassword', 'Salt']
+    
+
+    message =  sql.update(db, cursor, table, fields, keys, ['ViewerID'], ignore=ignore_update)
+    if fields['ViewerType'] != 'C' or fields['ViewerType'] != 'B':
+        message =  sql.update(db, cursor, 'CrowdFundingViewer', fields, keys_c, ['ViewerID'], ignore=ignore_update) 
+    if fields['ViewerType'] != 'P' or fields['ViewerType'] != 'B':
+        message =  sql.update(db, cursor, 'PremiumViewer', fields, keys_p, ['ViewerID'], ignore=ignore_update)          
+
+                         
+        
+######## If DELETE button is pressed then ... ###########################################################################        
+if form.getvalue("submit") == "Delete":
+    message = sql.delete(db, cursor, table, fields, ['ViewerID'])
+    
+        
+####### GENERATE AND EXECUTE SEARCH QUERY ##########################################################################
+
+# Search Viewer from Viewers using ViewerID and ignoring other fields    
+result =  sql.search(db, cursor, table, fields, keys, exact_keys, ignore=ignore_keys, limit=10, fetch_one=True)
+row = result[0];
+
+# If Viewer is found then check whether he/she is premium and/or crowdfunding and then search those table
+row_c = ["", "", "", ""]
+row_p = ["", ""]
+
+if row == None:
+    row = ["", "", "", "", "", "",  ""]
+    
+else:
+    if(row[1] == 'B' or row[1] == 'C'):
+        result =  sql.search(db, cursor, "CrowdFundingViewer", fields, keys_c, exact_keys, ignore=ignore_keys, limit=10, fetch_one=True)
+        row_c = result[0];
+        
+        if row_c == None:
+            row_c = ["", "", "", ""]
+
+    
+    if(row[1] == 'B' or row[1] == 'P'):
+        result =  sql.search(db, cursor, "PremiumViewer", fields, keys_p, exact_keys, ignore=ignore_keys, limit=10, fetch_one=True)
+        row_p = result[0];
+
+        if row_p == None:
+            row_p = ["", ""]
+
+        
+row = list(row)
+for i in range(7):
+    if row[i] == None:
+        row[i] = ""
+    else:
+        row[i] = escape(str(row[i]), entities)
+        
+row_c = list(row_c)
+for i in range(4):
+    if row_c[i] == None:
+        row_c[i] = ""
+    else:
+        row_c[i] = escape(str(row_c[i]), entities) 
+        
+row_p = list(row_p)
+for i in range(2):
+    if row_p[i] == None:
+        row_p[i] = ""
+    else:
+        row_p[i] = escape(str(row_p[i]), entities)
+
+
+
+    
+
+####### PRINT FORM ##############################################################################
     
 print html.make_head("video_modify.css", title="WWAG Viewers", extra_script=js)
 
@@ -60,61 +156,60 @@ print html.make_navbar(loggedIn, userType)
 print """
 <div class="search_form">
 <h2 class="header">VIEWERS</h2>
-<form name ="myForm" id="myForm" action="viewers.py" method="get">
+<form name ="myForm" id="myForm" action="viewer.py" method="get">
     <fieldset id="search">
         <legend>Maintain Viewer</legend>
         
         <div class="textbox">
             <label for="ViewerID">Viewer ID:</label>
-            <input name="ViewerID" id="ViewerID" type="text" />
+            <input name="ViewerID" id="ViewerID" type="hidden" value="{}"/>
+            <span class = "just_text">{}</span>
         </div>
         
          <div class="textbox">
             <label for="DateOfBirth">Date of Birth:</label>
-            <input name="DateOfBirth" id="DateOfBirth" type="date" />
+            <input name="DateOfBirth" id="DateOfBirth" type="date" value="{}"/>
         </div>
         
         <div class="textbox">
             <label for="Email">Email:</label>
-            <input name = "Email" id= "Email" type="text" />
+            <input name = "Email" id= "Email" type="text" value="{}"/>
         </div>
         
         <div class="textbox">
             <label for="Player Type">UserName:</label>
-            <input name="UserName" id="UserName" type="text" />
+            <input name="UserName" id="UserName" type="text" value="{}"/>
         </div>
 
         <div class="textbox">
-            <label for="HashedPassword">Password:</label>
+            <label for="HashedPassword">New Password:</label>
             <input name="HashedPassword" id="HashedPassword" type="text" />
         </div>
 
         <div class="textbox">
             <label for="ViewerType">Viewer Type:</label>
-            <input type="radio" name="ViewerType"  value="C" onclick="details();">Crowdfunding<br />
-            <input type="radio" name="ViewerType"  value="P" onclick="details();">Premium<br />
-            <input type="radio" name="ViewerType"  value="B" onclick="details();">Both<br />
-            <input type="radio" name="ViewerType"  value="N" checked="checked" onClick="details()">Regular
+            <input type="radio" name="ViewerType"  value="{}" checked="checked" onclick="details();">{}<br />
+
         </div>
         
         <div id = "crowd"><div class="textbox">
             <label for="FirstName">First Name:</label>
-            <input name="FirstName" id="FirstName" type="text" />
+            <input name="FirstName" id="FirstName" type="text" value="{}"/>
         </div>
 
         <div class="textbox">
             <label for="LastName">Last Name:</label>
-            <input name="LastName" id="LastName" type="text" />
+            <input name="LastName" id="LastName" type="text" value="{}"/>
         </div>
 
         <div class="textbox">
             <label for="TotalAmountDonated">Total Amount Donated:</label>
-            <input name="TotalAmountDonated" id="TotalAmountDonated" type="text" />
+            <input name="TotalAmountDonated" id="TotalAmountDonated" type="text" value="{}"/>
         </div></div>
 
         <div id = "premium"><div class="textbox">
             <label for="RenewalDate">RenewalDate:</label>
-            <input name="RenewalDate" id="RenewalDate" type="date" />
+            <input name="RenewalDate" id="RenewalDate" type="date" value="{}" />
         </div></div>
 
 
@@ -122,97 +217,22 @@ print """
     
     <div id="buttons" class="button_select">
         <input type="reset" value="Reset" />
-        <input type="submit" name="submit" value="Insert" />
-        <input type="submit" name="submit" value="Search" onClick="DoSubmit()" />
+        <input type="submit" name="submit" value="Update" />
+        <input type="submit" name="submit" value="Delete" />
     </div>
 </form>
 </div>
 <script type="text/javascript">
 details();
 </script>
-"""
-
-####### CONNECT TO DATABASE AND LOAD FORM DATA ##########################################################################
-
-db = MySQLdb.connect("info20003db.eng.unimelb.edu.au", "info20003g29", "enigma29", "info20003g29", 3306)
-cursor = db.cursor()
-table = 'Viewer'
-keys = ['ViewerID', 'ViewerType', 'DateOfBirth','Email', 'UserName', 'HashedPassword', 'Salt']
-ignore_keys = ['ViewerType', 'DateOfBirth','Email', 'UserName', 'HashedPassword', 'Salt']
-keys_c = ['ViewerID', 'FirstName', 'LastName', 'TotalAmountDonated']
-keys_p = ['ViewerID', 'RenewalDate']
-exact_keys = ['ViewerID', 'TotalAmountDonated']
-ignore = ['HashedPassword', 'Salt']
-
-fields = dict.fromkeys(keys + keys_c + keys_p)
-
-for key in fields:
-    fields[key] = form.getvalue(key)       
-
-######## If INSERT button is pressed then ... ###########################################################################
-message = ""
-stop = 0
-if form.getvalue("submit") == "Insert":
-    fields['Salt'] = uuid.uuid4().hex
-    if fields["HashedPassword"] != None:
-        fields["HashedPassword"] = hashlib.sha512(fields['HashedPassword'] + fields['Salt']).hexdigest()
-    else:
-        print '<div class = "error">Insert Error! Password is empty.</div>'
-        sys.exit(0);
-        print html.end_html      
-    
-    # Insert into main viewers table
-    message =  sql.insert(db, cursor, table, fields, keys)
-    if 'Error!' in message:
-        stop = 1
-    
-    if(fields['ViewerID'] == None):
-        fields['ViewerID'] = cursor.lastrowid        
-    
-    #Insert into subtypes if applicable and delete viewer if there is any error in inserting in subtypes table
-    if stop == 0 and (fields['ViewerType'] == 'B' or fields['ViewerType'] == 'C'):
-          message =  sql.insert(db, cursor, 'CrowdFundingViewer', fields, keys_c)
-          if 'Error!' in message:
-            sql.delete(db, cursor, table, fields, ['ViewerID'])
-            stop = 1
-
-        
-    if stop == 0 and (fields['ViewerType'] == 'B' or fields['ViewerType'] == 'P'):
-          message =  sql.insert(db, cursor, 'PremiumViewer', fields, keys_p)              
-          if 'Error!' in message:
-              sql.delete(db, cursor, table, fields, ['ViewerID'])
+""".format(row[0], row[0], row[2], row[3], row[4], row[1], row[1] , row_c[1], row_c[2], row_c[3], row_p[1], row_p[1])
+       
 
     
 print message                                                                                         
-print html.end_html
-
-####### GENERATE AND EXECUTE SEARCH QUERY  ################################################################################
-
-result =  sql.search(db, cursor, table, fields, keys, exact_keys, select=["ViewerID", "UserName"], ignore=ignore_keys, limit=10)
-
-rows = result[0];
-print result[1]; 
-    
-####### DISPLAY RESULTS TABLE  #############################################################################################
-print '<table class="gridtable" align="center">'
-
-# Print column headers    
-print '<tr>'
-print '<th>ViewerID</th><th>UserName</th>'
-print '</tr>'
-
-# Print each row of table    
-if rows != None: 
-    for row in rows:
-        print '<tr>'
-        print '<td><a href="viewer.py?ViewerID={0}">{0}</a></td><td><a href="viewer.py?ViewerID={0}">{1}</a></td>'.format(escape(str(row[0])), escape(str(row[1])))
-        print '</tr>'
-                                                                                             
-print '</table>'    
-                                                                                         
 print html.end_html
                      
 # Tidy up and free resources
 cursor.close()
 db.close()
-sess.close()    
+sess.close()  
